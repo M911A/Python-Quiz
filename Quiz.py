f28bdfc97265c01a6d39dd6c1e45b70bc41d1cab
Mika <93094528+M911A@users.noreply.github.com>
2022-07-24 13:28:15 +0200

    Add files via upload

---
+++ Quiz.py
@@ -0,0 +1,85 @@
+# -----This is my first Python Quiz-----
+
+def new_game():
+
+    guesses = []
+    correct_guesses = 0
+    question_num = 1
+
+    for key in questions:
+        print("-------------------------")
+        print(key)
+        for i in options[question_num-1]:
+            print(i)
+        guess = input("Enter (A, B, C, or D): ")
+        guess = guess.upper()
+        guesses.append(guess)
+
+        correct_guesses += check_answer(questions.get(key), guess)
+        question_num += 1
+
+    display_score(correct_guesses, guesses)
+
+# -----checking systhem-----
+def check_answer(answer, guess):
+
+    if answer == guess:
+        print("CORRECT!")
+        return 1
+    else:
+        print("WRONG!")
+        return 0
+
+# -----results-----
+def display_score(correct_guesses, guesses):
+    print("-------------------------")
+    print("RESULTS")
+    print("-------------------------")
+
+    print("Answers: ", end="")
+    for i in questions:
+        print(questions.get(i), end=" ")
+    print()
+
+    print("Guesses: ", end="")
+    for i in guesses:
+        print(i, end=" ")
+    print()
+
+    score = int((correct_guesses/len(questions))*100)
+    print("Your score is: "+str(score)+"%")
+
+# -----ending question----- 
+def play_again():
+
+    response = input("Do you want to play again? (yes or no): ")
+    response = response.upper()
+
+    if response == "YES":
+        return True
+    else:
+        return False
+
+# -----question & awnsers-----
+questions = {
+ "Which is the latest programming langugage?": "A", 
+ "Which unit of measurement from digital technology does it not?": "B",
+ "What does the abbreviation HTML stand for?": "C",
+ "What is a programming language?": "A"
+}
+
+options = [
+            ["A. Swift", "B. Python", "C. Java", "D. C#"],
+            ["A. Petabyte", "B. Zulerbvte", "C. Megabyte", "D. Terabyte"],
+            ["A. Hypertext Meta Language", "B. Hypertext Message Language", "C. Hypertext Markup Language", "D. Hypertext Model Language"],
+            ["A. Hollywood","B. Queens", "C. Spandau", "D. Westminster"]
+          ]
+
+new_game()
+
+while play_again():
+    new_game()
+
+print("Bye! Thaks for playing ;)")
+
+# -----end-----
